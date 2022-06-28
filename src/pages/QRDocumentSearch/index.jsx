4c28884e3ae542d786c4cb0d1e6f9415960ef271
Alexander <alexadedeji15@gmail.com>
2022-06-28 16:17:11 +0100

    added loaders

--- src/pages/QRDocumentSearch/index.jsx
+++ src/pages/QRDocumentSearch/index.jsx
@@ -5,36 +5,37 @@ import { fetchAttestedDocument } from "../../services/verifierService";
 import { toast } from "react-toastify";
 import NoFile from "../../component/NoFile";
 import QRDocumentSearchNavHeader from "./subComponent/QRDocumentSearchNavHeader";
+import DocumentLoader from "../../component/Spinner/DocumentLoader";
 
 const QRDocumentSearch = () => {
   const { documentRef } = useParams();
   const [documentVerified, setDocument] = useState(null);
   const [verifierState, setVerifierState] = useState({
-    btnLoader: false,
+    loading: true,
     noFile: false,
   });
 
   useEffect(() => {
     getDocument(documentRef);
-  }, []);
+  }, [documentRef]);
 
   const getDocument = async (searchData) => {
     setVerifierState((prevState) => ({
       ...prevState,
-      btnLoader: true,
+      loading: true,
     }));
     try {
       const { data } = await fetchAttestedDocument(searchData);
       setDocument(data);
       setVerifierState((prevState) => ({
         ...prevState,
-        btnLoader: false,
+        loading: false,
       }));
       toast.success("Document Found");
     } catch (errors) {
       setVerifierState((prevState) => ({
         ...prevState,
-        btnLoader: false,
+        loading: false,
       }));
       toast.error("Document Not Found");
     }
@@ -51,12 +52,8 @@ const QRDocumentSearch = () => {
     <div>
       <QRDocumentSearchNavHeader />
 
-      <div id="verified-document">
-        {verifierState.noFile ? (
-          <NoFile text="No Documents Found" />
-        ) : (
-          <DocumentLandingPage text="" />
-        )}
+      <div id="verified-document" className="mt-3">
+        {verifierState.loading ? <DocumentLoader /> : <NoFile />}
       </div>
     </div>
   );
